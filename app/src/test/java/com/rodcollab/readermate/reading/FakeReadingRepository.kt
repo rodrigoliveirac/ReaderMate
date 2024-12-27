9f2961e6332afc7512ed11a40770a7f867e60009
Rodrigo Oliveira <rodrigocavalcante.pro@gmail.com>
2024-12-27 18:58:59 -0300

    [RMATE-06] - refactor

--- app/src/test/java/com/rodcollab/readermate/reading/FakeReadingRepository.kt
+++ app/src/test/java/com/rodcollab/readermate/reading/FakeReadingRepository.kt
@@ -6,61 +6,15 @@ import com.rodcollab.readermate.core.model.toReading
 import com.rodcollab.readermate.core.model.toReadingEntity
 import com.rodcollab.readermate.features.reading.data.ReadingRepository
 import com.rodcollab.readermate.mockdatasource.MockDataSource
-import java.util.UUID
 
 class FakeReadingRepository(private val dao: MockDataSource<ReadingEntity>) : ReadingRepository {
-
-
-    init {
-        setupCompletedReadingsMock()
-    }
-
-    private fun setupCompletedReadingsMock() {
-        val newReading1 = Reading(
-            bookRecordId = "mock1",
-            goalPerDay = 10,
-            isCurrent = false,
-            startedDate = System.currentTimeMillis(),
-            estimatedEndDate = System.currentTimeMillis() + 1000L,
-            currentPage = 0,
-            isCompleted = true
-        )
-        val newReading2 = Reading(
-            bookRecordId = "mock2",
-            goalPerDay = 10,
-            isCurrent = false,
-            startedDate = System.currentTimeMillis(),
-            estimatedEndDate = System.currentTimeMillis() + 1000L,
-            currentPage = 400,
-            isCompleted = true
-        )
-
-        dao.createEntity(newReading1.toReadingEntity())
-        dao.createEntity(newReading2.toReadingEntity())
-    }
 
     /**
      * In the real repository, this method will use a dao.getCurrentReading() which is a query that returns
      * only the current reading based on `isCurrent` attribute.
      */
     override suspend fun getCurrentReading(): Reading? = try {
-        var currentReading = dao.getAll().find { it.isCurrent }?.toReading()
-        if(currentReading == null) {
-            val newReading = Reading(
-                uuid = UUID.randomUUID().toString(),
-                bookRecordId = "mock1",
-                goalPerDay = 10,
-                isCurrent = true,
-                startedDate = System.currentTimeMillis(),
-                estimatedEndDate = System.currentTimeMillis() + 1000L,
-                currentPage = 0,
-                isCompleted = false
-            ).toReadingEntity()
-            dao.createEntity(entity = newReading)
-            newReading.toReading()
-        } else {
-            currentReading
-        }
+        dao.getAll().find { it.isCurrent }?.toReading()
     } catch (e: Exception) {
         null
     }
--- app/src/test/java/com/rodcollab/readermate/reading/ReadingRepositoryTest.kt
+++ app/src/test/java/com/rodcollab/readermate/reading/ReadingRepositoryTest.kt
@@ -23,6 +23,7 @@ class ReadingRepositoryTest {
     @Test
     fun `When a new reading is added, it should be set as the current reading`() = runTest {
         // Given
+        setupCompletedReadingsMock(withCurrentReading = false)
         val newReading = Reading(
             bookRecordId = "mock1",
             goalPerDay = 10,
@@ -45,6 +46,7 @@ class ReadingRepositoryTest {
     @Test
     fun `When a reading is selected, the reading data should be get the uuid properly`() = runTest {
         // Given
+        setupCompletedReadingsMock(withCurrentReading = true)
         val readingList = dataSource.getAll()
 
         // When
@@ -59,6 +61,7 @@ class ReadingRepositoryTest {
     @Test
     fun `When the user finish a reading, isCurrent should be false and isCompleted should be true`() = runTest {
         // Given
+        setupCompletedReadingsMock(withCurrentReading = true)
         val currentReading = readingRepository.getCurrentReading()!!
 
         // When
@@ -75,6 +78,7 @@ class ReadingRepositoryTest {
     @Test
     fun `When the user delete a reading, it should not exist in the data source anymore`() = runTest {
         // Given
+        setupCompletedReadingsMock(withCurrentReading = true)
         val currentReading = readingRepository.getCurrentReading()!!
 
         // When
@@ -85,5 +89,43 @@ class ReadingRepositoryTest {
         val readingRemoved = readingRepository.getReadingById(readingId = currentReadingUuid)
 
         TestCase.assertNull(readingRemoved)
+    }
+
+    private suspend fun setupCompletedReadingsMock(withCurrentReading: Boolean = false) {
+        val newReading1 = Reading(
+            bookRecordId = "mock1",
+            goalPerDay = 10,
+            isCurrent = false,
+            startedDate = System.currentTimeMillis() - 20000L,
+            estimatedEndDate = System.currentTimeMillis() + 1000L,
+            currentPage = 0,
+            isCompleted = true
+        )
+        val newReading2 = Reading(
+            bookRecordId = "mock2",
+            goalPerDay = 10,
+            isCurrent = false,
+            startedDate = System.currentTimeMillis() - 10000L,
+            estimatedEndDate = System.currentTimeMillis() + 1000L,
+            currentPage = 400,
+            isCompleted = true
+        )
+
+        if(withCurrentReading) {
+            val currentReading = Reading(
+                bookRecordId = "mock2",
+                goalPerDay = 10,
+                isCurrent = true,
+                startedDate = System.currentTimeMillis(),
+                estimatedEndDate = System.currentTimeMillis() + 1000L,
+                currentPage = 400,
+                isCompleted = false
+            )
+
+            readingRepository.addReading(currentReading)
+        }
+
+        readingRepository.addReading(newReading1)
+        readingRepository.addReading(newReading2)
     }
 }
